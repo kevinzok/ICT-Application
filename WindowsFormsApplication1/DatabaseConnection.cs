0db957d61ba809ca27901fab4f28d849e4c4ead9
lazyallday <khoatran2794@gmail.com>
2015-11-16 15:22:58 +0200

    Add DatabaseConnection Class

---
+++ WindowsFormsApplication1/DatabaseConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1
+{
+    class DatabaseConnection
+    {
+        OleDbConnection myConnection = new OleDbConnection();
+        string str = "";
+
+    }
+}
